e3aa58e1265be9c61c181f83e59be308949f1e51
Aurelien "beorn" Rougemont <beorn@binaries.fr>
2020-01-15 13:48:56 +0100

    Fix bad response types for devices and virtual machines

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/beorn-/go-netbox
+
+go 1.13
+
+require (
+	github.com/digitalocean/go-netbox v0.0.0-20191226142847-a54653f8d8e1
+	github.com/go-openapi/errors v0.19.2
+	github.com/go-openapi/runtime v0.19.9
+	github.com/go-openapi/strfmt v0.19.4
+	github.com/go-openapi/swag v0.19.5
+	github.com/go-openapi/validate v0.19.3
+	github.com/netbox-community/go-netbox v0.0.0-20191226142847-a54653f8d8e1 // indirect
+)
--- netbox/client/dcim/dcim_devices_update_responses.go
+++ netbox/client/dcim/dcim_devices_update_responses.go
@@ -60,20 +60,20 @@ func NewDcimDevicesUpdateOK() *DcimDevicesUpdateOK {
 DcimDevicesUpdateOK dcim devices update o k
 */
 type DcimDevicesUpdateOK struct {
-	Payload *models.WritableDevice
+	Payload *models.WritableDeviceResponse
 }
 
 func (o *DcimDevicesUpdateOK) Error() string {
 	return fmt.Sprintf("[PUT /dcim/devices/{id}/][%d] dcimDevicesUpdateOK  %+v", 200, o.Payload)
 }
 
-func (o *DcimDevicesUpdateOK) GetPayload() *models.WritableDevice {
+func (o *DcimDevicesUpdateOK) GetPayload() *models.WritableDeviceResponse {
 	return o.Payload
 }
 
 func (o *DcimDevicesUpdateOK) readResponse(response runtime.ClientResponse, consumer runtime.Consumer, formats strfmt.Registry) error {
 
-	o.Payload = new(models.WritableDevice)
+	o.Payload = new(models.WritableDeviceResponse)
 
 	// response payload
 	if err := consumer.Consume(response.Body(), o.Payload); err != nil && err != io.EOF {
